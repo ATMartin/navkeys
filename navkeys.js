1e967caba7afa86afb3aa2a9863d3942e7174314
ATMartin <martin.alex.t@gmail.com>
2014-06-14 04:28:47 +0000

    Expanded keys hash and changed EventListener to check keyCode against all current keys in hash.

--- navkeys.js
+++ navkeys.js
@@ -2,24 +2,23 @@
  * NAVKEYS.JS
  * @ATMartin
  * First published...6/12/2014
- * Last update.......6/12/2014
+ * Last update.......6/13/2014
  *
  */
 
- var keys = {
- 	ARROW_LEFT: [37],
-	ARROW_UP: [38],
-	ARROW_RIGHT: [39],
-	ARROW_DOWN: [40]
- };
+var keys = {
+	37: ['ARROW_LEFT', '#nk_left'],
+	38: ['ARROW_UP', '#nk_up'],
+	39: ['ARROW_RIGHT', '#nk_right'],
+	40: ['ARROW_DOWN', '#nk_down']
+};
 
- var navThere = function(selector) {
- 	var destination = document.querySelector(selector).querySelector('a').getAttribute('href');
+var navThere = function(selector) {
+	var destination = document.querySelector(selector).querySelector('a').getAttribute('href');
 	window.location = destination
- }
+}
 
- document.addEventListener('keydown', function (k) {
- 	if (keys.ARROW_LEFT.indexOf(k.keyCode) !== -1) { navThere('#navkey_left'); }
-	else if (keys.ARROW_RIGHT.indexOf(k.keyCode) !== -1) { navThere('#navkey_right'); }
- }, false);
+document.addEventListener('keydown', function (k) {
+	if (keys[k.keyCode]) { navThere(keys[k.keyCode][1]) };
+}, false);
 
